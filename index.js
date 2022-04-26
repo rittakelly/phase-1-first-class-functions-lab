71282ded17160340a39d9a498e5ee05f253cfcb0
Ritta <brittanykelly33@gmail.com>
2022-04-26 02:00:55 -0400

    commit tree 2fac0c4bf48e22b1914880cf6de51c50322303ea

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0,2)
+}
+
+function returnLastTwoDrivers(drivers) {
+    return drivers.slice (2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+function createFareMultiplier(multiplier) {
+    return function(fare) {
+        return fare * multiplier
+    }
+}
+
+function fareDoubler(things) {
+    const doubles = createFareMultiplier(2) 
+    return doubles(things)
+    
+}
+
+function fareTripler(stuff) {
+    const triples = createFareMultiplier(3)
+    return triples(stuff)
+}
+
+function selectDifferentDrivers(drivers, func) {
+    return func(drivers)
+}
